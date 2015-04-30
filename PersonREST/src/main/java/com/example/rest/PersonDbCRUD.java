7f2026edd85d283e2b50ab7a80e2fc6656282814
gxvigo <gxvigo@gmail.com>
2015-04-30 14:31:58 +1200

    Completed PersonDbCrud with @POST method. Imported Postman collection with all the test clients built so far

--- PersonREST/src/main/java/com/example/rest/PersonDbCRUD.java
+++ PersonREST/src/main/java/com/example/rest/PersonDbCRUD.java
@@ -23,12 +23,16 @@ import org.json.simple.JSONObject;
 
 import com.example.model.Person;
 import com.example.utils.mongo.MongoDB;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.util.JSON;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
@@ -115,13 +119,13 @@ public class PersonDbCRUD {
     	System.out.println("### person: " + person);
     	
       	// check whether the personId already exist
-	
-    	int id = person.getPersonId();
 		
 		// Get a db connection to mongodb. Retrieve the given collection ("persons")
 		DBCollection coll = MongoDB.INSTANCE.getSomeDB().getCollection("persons");
 		
-		BasicDBObject query = new BasicDBObject("personId", id);
+    	int id = person.getPersonId();
+		
+    	BasicDBObject query = new BasicDBObject("personId", id);
 		DBCursor personCursor = coll.find(query);
 		System.out.println("### number of items returned by the query: " + personCursor.count());
 		
@@ -131,10 +135,16 @@ public class PersonDbCRUD {
 	        return Response.status(409).type("application/json").entity(msg).build();
 		}
 
-    	
-    	// add person document into mongodb
-    	
-        System.out.println(person.getPersonId());
+		// Convert Person object to JSON
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode personJson = mapper.valueToTree(person);		
+		
+		// Convert Person JSON to DBObject (mongodb)
+		DBObject personDb = (DBObject) JSON.parse(personJson.toString());
+		
+		// add person document into mongodb
+		coll.insert(personDb);
+    		
         return Response.status(201)
                 .contentLocation(new URI("/PersonREST/api/db-persons/" + person.getPersonId())).build();
     }
